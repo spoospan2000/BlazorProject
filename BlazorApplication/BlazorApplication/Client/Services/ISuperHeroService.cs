23b6f98f84bf4f23c9fbdcd61b19c277f77ed0b3
Spoorthy I <spoospan2000@gmail.com>
2022-03-29 17:48:26 +0530

    file added

---
+++ BlazorApplication/BlazorApplication/Client/Services/ISuperHeroService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApplication.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApplication.Client.Services
+{
+    public interface ISuperHeroService
+    {
+        event Action OnChange;
+        List<Comic> Comics { get; set; }
+        List<SuperHero> Heroes { get; set; }
+        Task<List<SuperHero>> GetSuperHeroes();
+        Task GetComics();
+        Task<SuperHero> GetSuperHero(int id);
+        Task<List<SuperHero>> CreateSuperHero(SuperHero hero);
+        Task<List<SuperHero>> UpdateSuperHero(SuperHero hero, int id);
+        Task<List<SuperHero>> DeleteSuperHero(int id);
+    }
+}
